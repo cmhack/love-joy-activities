d3ed2f6cd810a4ebc78b195f92171017133eac7a
MrConan <conanchanmh@hotmail.com>
2020-06-18 16:02:24 +0800

    Update Home page and add title

--- js/controller/HomeCtrl.js
+++ js/controller/HomeCtrl.js
@@ -4,7 +4,7 @@ app.controller('HomeCtrl', function($scope){
 	
 	$scope.soundON = true;
 	
-	var audio = new Audio('../audio/Fast Track.mp3');
+	var audio = new Audio('audio/Fast Track.mp3');
 	audio.play();
 	
 	$scope.playAudio = function(param){
@@ -16,5 +16,29 @@ app.controller('HomeCtrl', function($scope){
 			audio.pause();
 		}
 	}
+
+	var attempt = 3;
+
+	$scope.validate = function(){
+		var login = document.getElementById("login").value;
+		var password = document.getElementById("password").value;
+
+		if (login == "NJ001" && password == "312127"){
+			// alert("Login successfully");
+			window.location = "revision.html";
+			return false;
+		} 
+		else{
+			attempt --;
+			// alert("Wrong Login id or password!")
+			alert("Wrong login id or password! \n \n You still have left "+attempt+" attempt.");
+			if(attempt == 0){
+				document.getElementById("login").disabled = true;
+				document.getElementById("password").disabled = true;
+				document.getElementById("submit").disabled = true;
+				return false;
+				}
+			}
+		}
 
 });
